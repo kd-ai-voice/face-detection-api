a75537cd66198a5179dfe5e968ad9948cb66f864
kazem delsooz <128213134+kd-ai-voice@users.noreply.github.com>
2026-08-10 05:37:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,91 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
+import cv2
+import numpy as np
+from io import BytesIO
+from PIL import Image
 
-app = FastAPI()
+
+app = FastAPI(
+    title="AI Face Detection API"
+)
+
+
+face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    "haarcascade_frontalface_default.xml"
+)
 
 
 @app.get("/")
 def home():
     return {
-        "message": "Face Detection API Running"
+        "status": "AI Face Detection Running",
+        "developer": "Kazem Delsooz"
     }
+
+
+
+@app.post("/detect")
+async def detect_face(
+    file: UploadFile = File(...)
+):
+
+    image_bytes = await file.read()
+
+
+    image = Image.open(
+        BytesIO(image_bytes)
+    ).convert("RGB")
+
+
+    img = np.array(image)
+
+
+    gray = cv2.cvtColor(
+        img,
+        cv2.COLOR_RGB2GRAY
+    )
+
+
+    faces = face_detector.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5
+    )
+
+
+    result = img.copy()
+
+
+    for x,y,w,h in faces:
+
+        cv2.rectangle(
+            result,
+            (x,y),
+            (x+w,y+h),
+            (255,0,0),
+            3
+        )
+
+
+    output = Image.fromarray(result)
+
+
+    buffer = BytesIO()
+
+    output.save(
+        buffer,
+        format="JPEG"
+    )
+
+    buffer.seek(0)
+
+
+    return StreamingResponse(
+        buffer,
+        media_type="image/jpeg",
+        headers={
+            "faces-detected":str(len(faces))
+        }
+    )
