5e97e92c5c919e235237572c3fd42104c36cac81
kazem delsooz <128213134+kd-ai-voice@users.noreply.github.com>
2026-08-10 06:09:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,106 +1,28 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import StreamingResponse
-import cv2
-import numpy as np
-from io import BytesIO
-from PIL import Image
+import os
+import urllib.request
+from ultralytics import YOLO
 
 
-app = FastAPI(
-    title="AI Face Detection API",
-    version="1.0.0"
-)
+MODEL_PATH = "yolov8n-face.pt"
 
 
-# Load OpenCV face detector
-face_detector = cv2.CascadeClassifier(
-    cv2.data.haarcascades +
-    "haarcascade_frontalface_default.xml"
-)
+def download_model():
 
+    if not os.path.exists(MODEL_PATH):
 
-@app.get("/")
-def home():
-    return {
-        "status": "AI Face Detection API Running",
-        "developer": "Kazem Delsooz",
-        "version": "1.0.0"
-    }
+        print("Downloading YOLO face model...")
 
+        url = "LINK_DIRECT_MODEL"
 
-@app.get("/health")
-def health():
-    return {
-        "status": "healthy"
-    }
-
-
-@app.post("/detect")
-async def detect_face(file: UploadFile = File(...)):
-
-    # Read uploaded image
-    image_bytes = await file.read()
-
-    if not image_bytes:
-        return {
-            "error": "Empty image"
-        }
-
-    # Convert image bytes to PIL
-    image = Image.open(
-        BytesIO(image_bytes)
-    ).convert("RGB")
-
-    # PIL -> NumPy
-    img = np.array(image)
-
-    # RGB -> Gray
-    gray = cv2.cvtColor(
-        img,
-        cv2.COLOR_RGB2GRAY
-    )
-
-    # Detect faces
-    faces = face_detector.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
-
-    # Copy image
-    result = img.copy()
-
-    # Draw rectangles
-    for (x, y, w, h) in faces:
-
-        cv2.rectangle(
-            result,
-            (x, y),
-            (x + w, y + h),
-            (255, 0, 0),
-            3
+        urllib.request.urlretrieve(
+            url,
+            MODEL_PATH
         )
 
-    # Convert result to PIL
-    output = Image.fromarray(result)
+        print("Model downloaded successfully")
 
-    # Save to memory
-    buffer = BytesIO()
 
-    output.save(
-        buffer,
-        format="JPEG",
-        quality=95
-    )
+download_model()
 
-    buffer.seek(0)
 
-    # Return processed image
-    return StreamingResponse(
-        buffer,
-        media_type="image/jpeg",
-        headers={
-            "X-Faces-Detected": str(len(faces))
-        }
-    )
+model = YOLO(MODEL_PATH)
